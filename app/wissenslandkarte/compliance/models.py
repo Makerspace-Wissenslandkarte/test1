f386b0e21efd6427e3ba4d36ec79164e6246b7a7
Phillip Kuhrt <mail@phi1010.com>
2021-05-15 21:28:26 +0200

    Fix implicit primary key warning (models.W042)

--- app/wissenslandkarte/compliance/models.py
+++ app/wissenslandkarte/compliance/models.py
@@ -6,6 +6,8 @@ from django.db import models
 
 
 class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+
     class Meta:
         abstract = True  # specify this model as an Abstract Model
         app_label = 'compliance'
