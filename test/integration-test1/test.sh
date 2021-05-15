20e7a66441ee68610e87bf62bbb85d51782e2c48
Phillip Kuhrt <mail@phi1010.com>
2021-05-15 23:59:05 +0200

    Fix typo in integration test

--- test/integration-test1/test.sh
+++ test/integration-test1/test.sh
@@ -1,2 +1,2 @@
 set -e
-ping -c 10 python && curl http://python:8000/ | grep 'c0bbdbcd-957f-4d56-ac3c-  6c06ae06e27e' || (echo "ERROR: Failed to find integration test token in html header."; false)
+ping -c 10 python && curl http://python:8000/ | grep 'c0bbdbcd-957f-4d56-ac3c-6c06ae06e27e' || (echo "ERROR: Failed to find integration test token in html header."; false)
